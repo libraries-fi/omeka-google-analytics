5eb8c5ce7ca30cbb4390f8b8a96be6c31be5ea4b
Matti Lassila <matti.lassila@gmail.com>
2014-01-22 19:10:11 +0200

    Removed nested js tags which prevented GA from loading

--- GoogleAnalyticsPlugin.php
+++ GoogleAnalyticsPlugin.php
@@ -29,9 +29,8 @@ class GoogleAnalyticsPlugin extends Omeka_Plugin_AbstractPlugin
     public function hookPublicHead()
     {
         $gaKey = get_option('google_analytics_key'); // google analytics key
-        $analytics =<<< ANALYTICS
-<script>var _gaq=[['_setAccount','$gaKey'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=true;g.src=('https:'==location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s);})(document,'script');</script>
-ANALYTICS;
+        $analytics ="var _gaq=[['_setAccount','" . $gaKey . "'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=true;g.src=('https:'==location.protocol?'https://ssl':'http://www')+'.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s);})(document,'script');";
+
 
         queue_js_string($analytics);
     }
